0e74af31b61560bd962c52155b59306221fb3008
Nataša Minić <nminicn@gmail.com>
2021-07-03 19:20:25 +0200

    Dodavanje komponenti. Home page, About, Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+* @author
+* @function Header
+**/
+
+const Header = (props) => {
+  return(
+    <div>Header</div>
+   )
+
+ }
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+ReactDOM.render( //ispisuju - pokrece - sve sa stranice  /REACTDOM - data object model, render metoda služi za ispisivanje svih react elementara
+  <React.StrictMode> 
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); 
+//prvi parametar šta želimo da se ispiše, drugi parametar gde želimo da se ispiše
+//za definisanje konkretnih elemenata JSX - sličan html-u
+//konstanta: const element = <h1>Hello</h1>
+//u JSX možemo da menjamo tekst preko promenljivih 
+
+//const nesto = "Natasa";
+//<h1>Hello {nesto}</h1>
+
+//Unutar JSX možemo da stavimo celu funkciju
+
+//div className = "klasa"
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
