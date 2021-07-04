b9d8d7e64c462c710888fc4c8e1698827a4d06c3
Nataša Minić <nminicn@gmail.com>
2021-07-05 00:57:29 +0200

    Sidebar ažuriranje

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css';
 import Card from '../UI/Card';
+import logopic from '../../images/logo.png';
 
 /**
 * @author
@@ -9,9 +10,57 @@ import Card from '../UI/Card';
 
 const Sidebar = (props) => {
   return(
-    <Card style={{width:'27%', background:'white', borderRadius:'20px', borderStyle:'ridge'}}> 
-      Sidebar
-    </Card>
+
+    <div className = "sidebarContainer">
+
+      <Card style={{background:'white', borderRadius:'20px', border:'none', padding: '10px 2px', marginBottom: '20px',  boxSizing: 'border-box'}}> 
+        
+        <div className = "cardHeader">
+          <span>About Us</span>
+          <div className = "aboutUsImageContainer">
+            <img src={logopic}></img>
+          </div>
+
+          <div className = "cardBody">
+            <p className = "aboutUsBio">Svako žensko dete barem jednom u životu bude fascinirano lepim, našminkanim i negovanim ženama i želi kad poraste da bude baš kao one. Ljubav prema kozmetici i šminci učinila je da nastane ovaj blog. Naša želja je da podelimo sve naše savete i iskustva sa vama. Hvala što posećujete naš web sajt i nadamo se da ćemo opravdati Vaše poverenje! </p>
+          </div>
+
+        </div>
+
+        </Card>
+
+        <Card style={{background:'white', borderRadius:'20px', border:'none', padding: '10px',  boxSizing: 'border-box'}}> 
+        
+        <div className = "cardHeader">
+          <span>Social Network</span>
+        </div>
+
+        </Card>
+
+        <Card style={{background:'white', borderRadius:'20px', border:'none', padding: '10px',  boxSizing: 'border-box'}}> 
+        
+        <div className = "cardHeader">
+          <span>Recent Posts</span>
+        </div>
+
+        <div className = "recentPosts">
+          <div className = "recentPost">
+              <h3>Title</h3>
+              <span>date</span>
+          </div>
+
+          <div className = "recentPost">
+              <h3>Title</h3>
+              <span>date</span>
+          </div>
+
+        </div>
+
+        </Card>
+
+    </div>
+
+    
    )
 
  }
